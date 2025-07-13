e8eb6b3f28c19dfacd840a4c684c4fa7cf11231c
Richard Holland <richard@richardholland.com>
2025-07-13 15:51:21 +0100

    Initial Commit

---
+++ SwiftfulMap/SwiftfulMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapApp.swift
+//  SwiftfulMap
+//
+//  Created by Richard Holland on 13/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
